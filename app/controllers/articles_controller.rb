f681a6ed954922fb34b5432546db535864a0df1a
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-29 14:26:13 -0700

    Add edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
