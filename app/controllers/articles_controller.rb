284941cd17fe612b523f0cc4a3fba0ecaf75b02b
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-02-29 13:57:28 -0700

    Create new action and route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+
+  def show
+
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
 
+
 end
