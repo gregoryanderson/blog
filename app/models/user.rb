4abcccaf8bd4ed1f08344317b7257a78fced1c53
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-02 08:52:56 -0700

    Add assoc between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save {self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: {minimum: 3, maximum: 25}
@@ -7,4 +9,4 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }, 
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
-end
+ end
