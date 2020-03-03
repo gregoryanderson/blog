4e3e698936efca35bb98bd50b0beaffb9d23a559
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-03 14:18:00 -0700

    Add ability to create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Alpha Blog, #{@user.username}"
+      redirect_to articles_path
+    else 
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
