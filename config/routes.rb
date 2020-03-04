240ee37d7a309da56609d453bd1cc934005602bb
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-03 18:21:19 -0700

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   root 'welcome#home'
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
