feaf57949e6820721e1364415fd4fdc9e1974fd3
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-03 18:56:47 -0700

    Add sessions to application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-
-  end
-
-  def destroy
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
   end
   
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
+  end
+
 end
