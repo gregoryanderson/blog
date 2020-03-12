4a1a46cdcc225255481b0561ecd3b8c676a330b2
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-12 11:39:55 -0600

    Add test for listing categories index

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -17,7 +17,7 @@ class CreateCategoriesTest <  ActionDispatch::IntegrationTest
     get new_category_path 
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: "" } }
+      post categories_path , params: { category: { name: "" } }
     end 
     assert_template 'categories/new'
     assert_select 'h2.panel-title'
