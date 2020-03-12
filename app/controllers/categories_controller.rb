d547ce03170f7dec7430345f025b5ef19a6fc6d9
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-12 12:08:51 -0600

    Validate admins to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,59 @@
 class CategoriesController < ApplicationController
 
-  def new 
-    @category = Category.new
-  end 
-
+  before_action :require_admin, except: [:index, :show]
+  
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)
-  end 
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      flash[:success] = "Category was created successfully"
-      redirect_to categories_path
-    else 
-      render 'new'
-    end
+  
+  @categories = Category.paginate(page: params[:page], per_page: 5)
+  
   end
-
+  
+  def new
+  
+  @category = Category.new
+  
+  end
+  
+  def create
+  
+  @category = Category.new(category_params)
+  
+  if @category.save
+  
+  flash[:success] = "Category was created successfully"
+  
+  redirect_to categories_path
+  
+  else
+  
+  render 'new'
+  
+  end
+  
+  end
+  
   def show
-
+  
   end
   
   private
+  
   def category_params
-    params.require(:category).permit(:name)
-  end 
-end
+  
+  params.require(:category).permit(:name)
+  
+  end
+  
+  def require_admin
+  
+  if !logged_in? || (logged_in? and !current_user.admin?)
+  
+  flash[:danger] = "Only admins can perform that action"
+  
+  redirect_to categories_path
+  
+  end
+  
+  end
+  
+  end
