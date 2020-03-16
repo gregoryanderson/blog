c8aabc18c99d8988a3a64e8a9c568649b2fbc153
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-16 14:37:35 -0600

    Add categories to UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,8 +47,10 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
+    
+    
   def set_article
     @article = Article.find(params[:id])
   end 
