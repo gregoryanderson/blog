17ff67d9f54a96e54c59ecba335bf4187344f03b
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-17 11:07:49 -0600

    Allow admin to edit category names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,33 +3,35 @@ class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
   
   def index
-  
-  @categories = Category.paginate(page: params[:page], per_page: 5)
-  
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
   
   def new
-  
-  @category = Category.new
-  
+    @category = Category.new
   end
   
   def create
-  
-  @category = Category.new(category_params)
-  
-  if @category.save
-  
-  flash[:success] = "Category was created successfully"
-  
-  redirect_to categories_path
-  
-  else
-  
-  render 'new'
-  
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category was created successfully"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
   end
-  
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
   end
   
   def show
@@ -38,23 +40,15 @@ class CategoriesController < ApplicationController
   end
   
   private
-  
   def category_params
-  
-  params.require(:category).permit(:name)
-  
+    params.require(:category).permit(:name)
   end
-  
+
   def require_admin
-  
-  if !logged_in? || (logged_in? and !current_user.admin?)
-  
-  flash[:danger] = "Only admins can perform that action"
-  
-  redirect_to categories_path
-  
+    if !logged_in? || (logged_in? and !current_user.admin?)
+    flash[:danger] = "Only admins can perform that action"
+    redirect_to categories_path
+    end
   end
   
-  end
-  
-  end
+end
